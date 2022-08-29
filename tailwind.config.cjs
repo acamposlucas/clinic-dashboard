6cde8f82b671f8f73d87253359766c2daf381196
lucas <lac.lucas96@gmail.com>
2022-08-29 15:17:33 -0300

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				"gray-900": "#464646",
+				"gray-400": "#A3A3A3",
+				"gray-300": "#A3A3A3",
+				"gray-200": "#F6F6F6",
+				"blue-500": "#4894FF",
+				"white-100": "hsla(100, 100%, 100%, 0.3)",
+				"yellow-500": "#FFB050",
+			},
+			boxShadow: {
+				default: "0px 4px 16px rgba(72, 148, 255, 0.16)",
+			},
+			fontFamily: {
+				body: ["Montserrat", "sans-serif"],
+			},
+			fontSize: {
+				sm: [
+					"clamp(0.75rem, 0.705rem + 0.23vw, 0.875rem)",
+					{
+						lineHeight: 1.5,
+					},
+				],
+				base: [
+					"clamp(0.875rem, 0.83rem + 0.23vw, 1rem)",
+					{
+						lineHeight: 1.5,
+					},
+				],
+				"2xl": [
+					"clamp(1.375rem, 1.148rem + 1.14vw, 2rem)",
+					{
+						lineHeight: 1.6,
+					},
+				],
+			},
+		},
+	},
+	plugins: [],
+};
