0ac0d37e5ea3a88a285079afd7d20b94d525a75e
lucas <lac.lucas96@gmail.com>
2022-08-29 17:14:53 -0300

    fix: something was broken works now

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,10 @@
 import { ReactNode } from "react";
+import { Header } from "./Header";
 
 export const Layout = ({ children }: { children: ReactNode }) => {
 	return (
-		<div className="font-body max-w-[1200px] mx-4 lg:mx-auto lg:mx-0 bg-white-100">
-			{children}
+		<div className="font-body max-w-[1200px] mx-4 lg:mx-auto bg-white-100">
+			<Header />
 		</div>
 	);
 };
